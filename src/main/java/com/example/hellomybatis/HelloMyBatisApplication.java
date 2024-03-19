059a9dcee356cd78d2d97771537b8069eb66b52a
Kevin <kevinsmith2001233@gmail.com>
2024-03-19 14:43:44 +0800

    Initial commit

---
+++ src/main/java/com/example/hellomybatis/HelloMyBatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellomybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMyBatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMyBatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellomybatis/HelloMyBatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellomybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMyBatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
